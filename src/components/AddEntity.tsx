4dc592ffbef7d073522886e4449d6158f3cfb2b4
Al-Amin49 <alamin496832@gmail.com>
2024-11-29 12:47:33 +0600

    feat: initial setup done

---
+++ src/components/AddEntity.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddEntity = () => {
+    return (
+        <div>
+            <button className=' px-2 py-3'>Add Animal</button>
+            <button >Add Category</button>
+        </div>
+    );
+};
+
+export default AddEntity;
